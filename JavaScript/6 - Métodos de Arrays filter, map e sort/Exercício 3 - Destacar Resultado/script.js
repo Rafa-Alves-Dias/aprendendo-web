56634b6c73c49b20b71eab9318133e9e69e48b1c
Rafa-Alves-Dias <rafael.dias.programador@gmail.com>
2026-04-30 15:10:35 -0300

    Exercício 2 feito e exercício 3 com erros graves
    Alguns erros do exercício 3: erro de posicionamento de onde deveria ocorrer a mudança da coloração, erros conceituais como confundir uma array com  um objeto, uso incorreto do "classList" em uma string diretamente.

--- JavaScript/6 - Métodos de Arrays filter, map e sort/Exercício 3 - Destacar Resultado/script.js
+++ JavaScript/6 - Métodos de Arrays filter, map e sort/Exercício 3 - Destacar Resultado/script.js
@@ -11,9 +11,9 @@ let produtos = [
     { nome: "Webcam Full HD", preco: 250.00, estoque: 45, categoria: "Periféricos" },
     { nome: "Headset Gamer", preco: 180.00, estoque: 60, categoria: "Periféricos" },
     { nome: "SSD 1TB", preco: 450.00, estoque: 80, categoria: "Eletrônicos" },
-    { nome: "Mousepad RGB", preco: 85.00, estoque: 120, categoria: "Periféricos",
-        nome: "Apple Watch SE (3ª geração)", preco:  2399.90, estoque: 120, categoria: "Eletrônicos"
-     }
+    { nome: "Mousepad RGB", preco: 85.00, estoque: 120, categoria: "Periféricos"},
+    {nome: "Apple Watch SE (3ª geração)", preco:  2399.90, estoque: 0, categoria: "Eletrônicos"
+    }
 ];
 
 // 2. BUSCAR ELEMENTOS
@@ -26,6 +26,9 @@ let contador = document.getElementById("contador");
 
 // 1.3 - cria o DOM do select de preço
 let selectFaixaPreco = document.getElementById("faixaPreco");
+
+// 2.3 - checkbox de estoque
+let checkboxEstoque = document.getElementById("apenasEstoque");
 
 // ========================================
 // 3. FUNÇÃO: EXIBIR PRODUTOS
@@ -45,10 +48,17 @@ function exibirProdutos(arrayProdutos = produtos) {
     arrayProdutos.forEach(function(produto) {
         let card = document.createElement("div");
         card.className = "card-produto";
+        // 2.0 - formatar o preço para o padrão brasileiro
+        const formatadorBR = new Intl.NumberFormat('pt-BR',{
+            style: 'currency', //adficiona na frente o simbolo da moeda
+            currency: 'BRL', // seleciona o simbolo de reais (R$)
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2 //fixa o número de casas decimais em 2 casas
+        });
 
         card.innerHTML = `
             <div class="produto-nome">${produto.nome}</div>
-            <div class="produto-preco">R$ ${produto.preco.toFixed(2)}</div>
+            <div class="produto-preco">${formatadorBR.format(produto.preco)}</div>
             <div class="produto-estoque">Estoque: ${produto.estoque} unidades</div>
             <span class="produto-categoria">${produto.categoria}</span>
         `;
@@ -69,7 +79,7 @@ function filtrarProdutos() {
     let ordenacao = selectOrdenar.value;
     // 1.4 - variavel de filtro de preco
     let valorLimite =  selectFaixaPreco.value;
-    
+
     // BOA PRÁTICA: sempre por os filtros antes de ordena-los
 
     // COMEÇAR COM TODOS OS PRODUTOS
@@ -81,13 +91,18 @@ function filtrarProdutos() {
             return produto.nome.toLowerCase().includes(textoBusca);
         });
     }
-
+    
+    // 3.3 - colorindo o texto procurado
+    resultado.nome = resultado.nome.replace(new RegExp(textoBusca, 'gi'), function(){
+        return resultado.nome.classList.add("fonte-amarela");
+    }); 
     // FILTRAR POR CATEGORIA (usando .filter()!)
     if (categoriaSelecionada !== "todos") {
         resultado = resultado.filter(function(produto) {
             return produto.categoria === categoriaSelecionada;
         });
     }
+    
     // 1.5 - filtro por preco
     if(valorLimite !=="nenhum"){
         switch(valorLimite){
@@ -101,6 +116,9 @@ function filtrarProdutos() {
                 resultado = resultado.filter(produto => produto.preco >= 500);
                 break
         }
+    }
+    if(checkboxEstoque.checked){
+        resultado = resultado.filter(produto => produto.estoque > 0);
     }
     // ORDENAR (usando .sort()!)
     if (ordenacao === "nome") {
@@ -131,6 +149,7 @@ function limparFiltros() {
     selectCategoria.value = "todos";
     selectOrdenar.value = "padrao";
     selectFaixaPreco.value = "nenhum";
+    checkboxEstoque.checked = false;
     exibirProdutos(produtos);
 }
 
@@ -142,6 +161,8 @@ selectCategoria.addEventListener("change", filtrarProdutos);
 selectOrdenar.addEventListener("change", filtrarProdutos);
 // 1.6 - lsitener para o filtro de preco
 selectFaixaPreco.addEventListener("change", filtrarProdutos);
+// 2.5 - eventListener do checkbox
+checkboxEstoque.addEventListener("change", filtrarProdutos);
 btnLimpar.addEventListener("click", limparFiltros);
 
 // ========================================
